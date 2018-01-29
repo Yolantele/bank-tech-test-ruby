e9ec570da39f8da78f9de175c273d3c946346419
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-29 11:50:20 +0000

    Stamps transaction with current time or given date

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction
+  attr_reader :date
 
+  def initialize(date = Time.new)
+    @date = date
+  end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,11 +2,19 @@ require 'transaction'
 
 describe Transaction do
 
-  subject(:transaction) { described_class.new }
+  subject(:transaction) { described_class.new('2018-01-29')}
 
-  describe 'iniitialisation' do
+  describe 'initialisation' do
     it 'is an instance of transaction' do
       expect(transaction).to be_an_instance_of(Transaction)
     end
+    it 'timestamps with current date' do
+      t = Transaction.new('2018-01-29')
+      expect(t.date).to eq('2018-01-29')
+    end
   end
+  # describe 'withdraw' do
+  #   it 'deducts money' do
+  #     expect(transaction.)
+  # end
 end
