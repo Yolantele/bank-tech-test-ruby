030506316bdaa082efd1c6b7463eae29535e9c32
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-29 12:13:50 +0000

    Extracts timeStamp method and completes transaction information

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,12 +1,22 @@
 class Transaction
   attr_reader :date, :transaction
 
-  def initialize(date = Time.new)
-    @date = date
+  def initialize()
+    @date = 0
     @transaction = 0
   end
 
+  def time_stamp(date = Time.new)
+    @date = date
+  end
+
   def withdraw(sum)
+    time_stamp
     @transaction -= sum
+  end
+
+  def deposit(sum)
+    time_stamp
+    @transaction += sum
   end
 end
